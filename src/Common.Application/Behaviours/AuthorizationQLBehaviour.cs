94b9d659f8c7e3cffb585f241100a91cec8f153f
Sergio Hernandez <sergio.hernandez.p@gmail.com>
2023-11-29 23:57:58 -0500

    Implemented Authorization Behaviour for GraphQL

---
+++ src/Common.Application/Behaviours/AuthorizationQLBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using Common.Application.Exceptions;
+using Common.Application.Interfaces;
+
+namespace Common.Application.Behaviours;
+
+public class AuthorizationQLBehaviour<TRequest, TResponse>(
+    IUser user,
+    IIdentityService identityService) : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var authorizeAttributes = request.GetType().GetCustomAttributes<AuthorizeQLAttribute>();
+
+        if (authorizeAttributes.Any())
+        {
+            // Must be authenticated user
+            if (user.Id == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            // Role-based authorization
+            var authorizeAttributesWithRoles = authorizeAttributes.Where(a => a.Roles != null && a.Roles.Length != 0);
+
+            if (authorizeAttributesWithRoles.Any())
+            {
+                var authorized = false;
+
+                foreach (var roles in authorizeAttributesWithRoles.Select(a => a.Roles))
+                {
+                    if (roles != null)
+                    {
+                        foreach (var role in roles)
+                        {
+                            var isInRole = identityService.IsInRoleAsync(role.Trim());
+                            if (isInRole)
+                            {
+                                authorized = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // Must be a member of at least one role in roles
+                if (!authorized)
+                {
+                    throw new ForbiddenAccessException();
+                }
+            }
+
+            // Policy-based authorization
+            var authorizeAttributesWithPolicies = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Policy));
+            if (authorizeAttributesWithPolicies.Any())
+            {
+                foreach (var policy in authorizeAttributesWithPolicies.Select(a => a.Policy))
+                {
+                    if (policy != null)
+                    {
+                        var authorized = identityService.AuthorizeAsync(policy);
+
+                        if (!authorized)
+                        {
+                            throw new ForbiddenAccessException();
+                        }
+                    }
+                }
+            }
+        }
+
+        // User is authorized / authorization not required
+        return await next();
+    }
+}
---
+++ src/Common.Application/Security/AuthorizeQLAttribute.cs
@@ -0,0 +1,14 @@
+﻿using HotChocolate.Authorization;
+
+
+/// <summary>
+/// Specifies the class this attribute is applied to requires authorization.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+public class AuthorizeQLAttribute : AuthorizeAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthorizeAttribute"/> class. 
+    /// </summary>
+    public AuthorizeQLAttribute() { }
+}
