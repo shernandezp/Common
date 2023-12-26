7167f2c70c43b7cd8ee9c2d0f6ae9557218bd91b
Sergio Hernandez <sergio.hernandez.p@gmail.com>
2023-12-25 22:52:20 -0500

    Removed custom salt for BCrypt

--- src/Common.Domain/Extensions/DateExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Common.Domain.Extensions;
-
-public static class DateExtensions
-{
-    public static string FormatPasswordReset(this DateTime value)
-        => string.Format("{0:yyyy-MM-dd HH:mm:ss.fffff}", value);
-}
--- src/Common.Domain/Extensions/PasswordExtensions.cs
+++ src/Common.Domain/Extensions/PasswordExtensions.cs
@@ -2,8 +2,8 @@
 
 public static class PasswordExtensions
 {
-    public static string HashPassword(this string value, DateTime passwordReset) 
-        => BCrypt.Net.BCrypt.HashPassword(value, passwordReset.FormatPasswordReset());
+    public static string HashPassword(this string value)
+        => BCrypt.Net.BCrypt.HashPassword(value);
 
     public static bool VerifyHashedPassword(this string hashedPassword, string password)
         => BCrypt.Net.BCrypt.Verify(password, hashedPassword);
